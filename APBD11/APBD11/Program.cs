d2a9a61b446bc358c91dea6494868c8a2f21d988
20RomKaSoul <s32880@pjwstk.edu.pl>
2026-05-27 19:50:17 +0200

    Config modification

--- APBD11/APBD11/Program.cs
+++ APBD11/APBD11/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using APBD11.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<Apbd11HwContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
